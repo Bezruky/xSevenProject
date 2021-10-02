e451a80f63868f08ea22ca111051818a50923f27
artemy <petra.poorino@gmail.com>
2021-10-02 16:58:32 +0300

    Добавление слайдера

--- js/script.js
+++ js/script.js
@@ -33,4 +33,21 @@ list.addEventListener('click', () => {
   }
 })
 
+// Slider
+new Swiper('.swiper', {
+
+  pagination: {
+    el: '.swiper-pagination',
+    clickable: true,
+  },
+
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+  },
+
+  loop: true,
+
+});
+
 
